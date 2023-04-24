113a66598590bad816231332cb592893aaf3d7a1
TQtong <2733707740@qq.com>
2023-04-24 15:14:58 +0800

    Feat: add cesium combine three

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,24 @@
  * @Author: TQtong 2733707740@qq.com
  * @Date: 2023-04-24 09:54:07
  * @LastEditors: TQtong 2733707740@qq.com
- * @LastEditTime: 2023-04-24 11:05:37
+ * @LastEditTime: 2023-04-24 15:12:23
  * @FilePath: \three-cesium-demo\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <!-- <CesiumLoading/> -->
-  <CesiumUseThree/>
+  <CesiumUseThreeStart/>
 </template>
 
 <script lang='ts'>
 import { Options, Vue } from 'vue-class-component'
 import CesiumLoading from './views/CesiumLoading/CesiumLoading'
-import CesiumUseThree from './views/CesiumUseThree/CesiumUseThree'
+import CesiumUseThreeStart from './views/CesiumUseThreeStart/CesiumUseThreeStart'
 
 @Options({
   components: {
     // CesiumLoading,
-    CesiumUseThree
+    CesiumUseThreeStart
   }
 })
 export default class App extends Vue {}
