65d939890883fddf977ae4aec550b1205540665e
TQtong <2733707740@qq.com>
2023-07-17 09:47:02 +0800

    Feat: add base setting

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: TQtong 2733707740@qq.com
  * @Date: 2023-04-24 09:54:07
  * @LastEditors: TQtong 2733707740@qq.com
- * @LastEditTime: 2023-05-05 17:10:51
+ * @LastEditTime: 2023-07-05 11:41:59
  * @FilePath: \three-cesium-demo\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -16,11 +16,6 @@
 
 <script lang='ts' setup>
 import { onMounted } from 'vue'
-import { webSocketInit } from '@/utils/ueMap'
-
-onMounted(() => {
-  webSocketInit()
-})
 
 </script>
 
